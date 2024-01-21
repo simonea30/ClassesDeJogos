b2702adb095344dcf86d7bb213a52288127b5d53
simonea30 <103963829+simonea30@users.noreply.github.com>
2024-01-20 22:09:30 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,58 @@
+
+class heroi{
+
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar(){
+        
+        if (this.tipo == 'mago'){
+            console.log(`O ${this.tipo} atacou usando magia.`);     
+        }
+
+        else if (this.tipo == 'guerreiro'){
+            console.log(`O ${this.tipo} atacou usando espada`);     
+        }
+
+        else if (this.tipo == 'monge'){
+            console.log(`O ${this.tipo} atacou usando marciais`);  
+
+        } else {
+
+        console.log(`O ${this.tipo} atacou usando shuriken`);
+        }
+    }
+
+}
+const heroi1 = new heroi('Mestre dos Magos', 35, 'mago');
+
+heroi1.atacar();
+
+/*
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada
+  */
